8540066a7cb6a3769419e1574ef8b5b5cc1c4fa2
jitu <jitendra.tiwari2@t-mobile.com>
2024-01-01 20:40:08 +0530

    write test cases and add book finished indicator for mark book as read

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -11,4 +11,22 @@ describe('When: I use the reading list feature', () => {
       'My Reading List'
     );
   });
+
+  // new test case to mark a book as finished
+  it('Then: I should see book as finished', () => {
+    cy.get('input[type="search"]').type('javascript');
+    cy.get('form').submit();
+    cy.get('[data-testing="want-to-read-button"]').click({ multiple: true ,force: true});
+
+    cy.get('[data-testing="toggle-reading-list"]').click({ multiple: true })
+
+    cy.get('[data-testing="mark-as-read"]').click({ multiple: true,force: true });
+
+    cy.get('[data-testing="finished-book-icon"]').should('exist');
+
+    cy.get('[data-testing="reading-list-container"]').first().as('close').click({ multiple: true });
+
+    cy.get('[data-testing="want-to-read-button"]').contains('Finished',{force: true });
+  });
+
 });
